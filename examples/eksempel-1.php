23ce8e2f9d36eb5cef96bfb3d285fd2d878e0f36
frankbonafide <frlud6323@usn.no>
2025-08-26 20:53:24 +0200

    Update eksempel-1.php

--- examples/eksempel-1.php
+++ examples/eksempel-1.php
@@ -1,15 +1,10 @@
-<?php    /* Eksempel 2 */
+<?php    /* Eksempel 1 */
 /*
-/*    Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
-/*    Programmet skriver ut de 2 tallene og summen og differansen av/mellom tallene
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
-  $tall1=$_POST ["tall1"];
-  $tall2=$_POST ["tall2"];  
-
-  $summen=$tall1 + $tall2;  
-  $differansen=$tall1 - $tall2;  
-			
-  print ("Tall 1 er $tall1 <br />");
-  print ("Tall 2 er $tall2 <br />");
-  print ("Summen er $summen <br />");
-  print ("Differansen er $differansen <br />"); 
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  
+	
+  print ("God dag $fornavn $etternavn <br />");  
+?>
