22f31f4c31f637e1fe7265293236e8681d52c0a3
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-06 15:59:05 +0200

    hmm

--- tema01/eksempler/eksempel-1.php
+++ tema01/eksempler/eksempel-1.php
@@ -1,6 +1,6 @@
 <?php
-$fornavn=$_POST ["fornavn" ]
-$etternavn=$_POST ["etternavn"]
+ $fornavnfornavn=$_POST ["fornavn" ]
+ $etternavn=$_POST ["etternavn"]
 
 print ("God dag $fornavn $etternavn! <br />");
 ?>
