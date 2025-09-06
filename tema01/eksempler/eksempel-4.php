f7988423981856e7881db4b8faedadbf852ca705
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-06 16:28:20 +0200

    eks.4

---
+++ tema01/eksempler/eksempel-4.php
@@ -0,0 +1,14 @@
+<?php
+$tall1=$POST_ ["tall1"];
+$tall2=$POST_ ["tall2"]; 
+$summen=$tall1 + $tall2;
+$differansen=$tall1 - $tall2;
+$produkt=$tall1 * $tall2;
+$kviotent=$tall1 / $tall2;
+
+print ("summen er $summen <br />");
+print ("differansen er $differansen <br />");
+print ("produktet er $produkt <br />");
+print ("kviotenten er $kviotent <br />");
+?>
+
