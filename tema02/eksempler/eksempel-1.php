dcdf21ca2ccda2488a26a948253fa542f7cbbbc5
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-07 11:28:49 +0200

    tema02 eks1

---
+++ tema02/eksempler/eksempel-1.php
@@ -0,0 +1,9 @@
+<?php
+  $svar=$_POST ["svar" ];
+    if ($svar=="9") {
+        print ("Riktig svar! <br />");
+    }
+    else {
+        print ("Feil svar, 3 x 3 er ikke $svar. <br />");
+    }
+?>
