d980c48d244476b103b61ec53d6c2b69bae520c9
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-12 18:14:47 +0200

    fe

---
+++ tema02/eksempler/eksempel-2.php
@@ -0,0 +1,14 @@
+<?php
+    $svar=$_POST ["svar"];
+        if ($svar=="j") {
+            print ("du har svart ja, på spørsmålet om du er student");
+            ");
+        }
+        if  ($svar=="n") {
+            print ("Du har svart neit, på spørsmålet om du er student");
+        }
+        if ($svar!)
+        {
+            print ("Du har ikke svart på spørsmålet om du er student");
+        }
+?>
