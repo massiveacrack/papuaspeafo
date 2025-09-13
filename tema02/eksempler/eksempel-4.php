597763d4a6e0d6ca93a2dea9bddd0697709f9fc8
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-13 11:28:03 +0200

    fefefefe

--- tema02/eksempler/eksempel-4.php
+++ tema02/eksempler/eksempel-4.php
@@ -11,3 +11,19 @@
         print ("Du er gift og har barn <br />");
     }
   
+    if ($gift=="j" && $barn=="n") {
+        print ("Du er gift og har ikke barn <br />");
+    }
+
+    if ($gift=="n" && $barn=="j") {
+        print ("Du er ikke gift og har barn <br />");
+    }
+
+    if ($gift=="n" && $barn=="n") {
+        print ("Du er ikke gift og har ikke barn <br />");
+    }
+
+    else {
+        print ("Ugyldig svar, vennligst svar med j eller n <br />");
+    }
+?>
