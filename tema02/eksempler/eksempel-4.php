a11499c3b26059d785dc174100e270ddfda9ba5d
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-13 11:26:17 +0200

    upload

--- tema02/eksempler/eksempel-4.php
+++ tema02/eksempler/eksempel-4.php
@@ -1,6 +1,13 @@
 <?php
  $gift=$_POST ["gift"];
  $barn=$_POST ["barn"];
+
+    if ($gift=="" || $barn=="") {
+        print ("Du har ikke svart på alle spørsmålene, vennligst prøv igjen! <br />");
+    }
+
+
     if ($gift=="j" && $barn=="j") {
         print ("Du er gift og har barn <br />");
     }
+  
