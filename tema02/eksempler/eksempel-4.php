7d1e039f817885414a39255a21766ac5e599ea5c
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-13 11:24:17 +0200

    defe

--- tema02/eksempler/eksempel-4.php
+++ tema02/eksempler/eksempel-4.php
@@ -1,6 +1,6 @@
 <?php
  $gift=$_POST ["gift"];
  $barn=$_POST ["barn"];
-    if ($gift=="j" || $barn=="j") {
+    if ($gift=="j" && $barn=="j") {
         print ("Du er gift og har barn <br />");
     }
