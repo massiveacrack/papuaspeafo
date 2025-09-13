c605013695ce5d4501abae818e26342d6ffad43f
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-13 11:58:53 +0200

    oppgave-6

---
+++ tema02/eksempler/eksempel-6.php
@@ -0,0 +1,26 @@
+<?php
+    $tall1=$_POST ["tall1"];
+    $tall2=$_POST ["tall2"];
+
+        if ("$tall1=="" || $tall2==") {
+            print ("Du har ikke tastet inn begge tallene, vennligst prøv igjen! <br />");
+            exit;
+        }
+
+        else if ($tall1 > $tall2) {
+            print ("$tall1 er høyere enn $tall2 <br />");
+        }
+
+        else if ($tall1 < $tall2) {
+            print ("$tall2 er høyere enn $tall1 <br />");
+        }
+
+        else if ($tall1 == $tall2) {
+            print ("$tall1 og $tall2 er like verdi <br />");
+        }
+
+        else {
+            print ("Ugyldig input, vennligst tast inn to tall <br />");
+            exit;
+        }
+    
