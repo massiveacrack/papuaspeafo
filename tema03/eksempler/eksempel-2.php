8b1462c4433666e6300671ac0c80db2569cf5807
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 16:06:47 +0200

    gogogogo

--- tema03/eksempler/eksempel-2.php
+++ tema03/eksempler/eksempel-2.php
@@ -2,8 +2,6 @@
 
 for ($tall=1;$tall<=10;$tall++)
     
-
-
 {
     $kvadrat = ($tall*$tall);
     print("Kvadratrot av $tall er lik $kvadrat <br />");
---
+++ tema03/eksempler/eksempel-3.php
@@ -0,0 +1,20 @@
+<?php
+
+for ($tall=1;$tall<=10;$tall++)
+
+    {
+        print("$tall <br />");
+    }
+
+for ($tall=11;$tall<=20;$tall++)
+
+    { 
+        print("$tall <br />");
+    }
+
+for ($tall=21;$tall<=30;$tall++)
+
+    {
+        print("$tall <br />");
+    }
+?>
