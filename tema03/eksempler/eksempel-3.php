5496bc0271b79169acfd66d484dcafcfae30feda
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 16:13:05 +0200

    weird

--- tema03/eksempler/eksempel-3.php
+++ tema03/eksempler/eksempel-3.php
@@ -5,20 +5,20 @@ for ($tall=1;$tall<=10;$tall++)
     {
         print("$tall <br />");
     }
-print("<br />);
+print("<br />");
 
 for ($tall=11;$tall<=20;$tall++)
 
     { 
         print("$tall <br />");
     }
-print("<br />);
+print("<br />");
 
 for ($tall=21;$tall<=30;$tall++)
 
     {
         print("$tall <br />");
     }
-print("<br />);
+print("<br />");
 
 ?>
