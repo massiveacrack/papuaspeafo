7ce7d4116820bcbfe0368154fbfd281190b179ae
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 16:56:49 +0200

    excpot

--- tema03/eksempler/eksempel-6.php
+++ tema03/eksempler/eksempel-6.php
@@ -1,10 +1,17 @@
 <?php
 
-$tall=$_POST["tall"];
-
-
-for ($tall=1;$tall<=$tall;$tall++)
+$angittTall=$_POST["angittTall"];
+if ($angittTall>0)
+{
+    print("Du ikke valgt en positiv og hell tall" <br />);
+}
+else ($angittTall=<0)
+{ 
+    for ($tall=1;$tall<=$angittTall;$tall++)
     {
-    print("$tall ");
-    print("<br />");
+    print("$tall <br />");
     }
+}
+
+?>
+    
