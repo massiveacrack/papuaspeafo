5227246d3101327adefe63389abbb7ad10a9f73c
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 15:16:29 +0200

    testfy

--- tema03/eksempler/eksempel-2.php
+++ tema03/eksempler/eksempel-2.php
@@ -1,10 +1,11 @@
 <?php
 
 for ($tall=1;$tall<=10;$tall++)
+    $tallrot=sqrt($tall);
 
 
 {
-    print("Kvadratrot av $tall er lik $tall*$tall <br />");
+    print("Kvadratrot av $tall er lik $tallrot <br />");
 }
 
 ?>
