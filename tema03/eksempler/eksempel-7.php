4d9c6ec5adf35e127fa6f5dc047e633e7eb1957e
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 17:23:42 +0200

    vretfs

--- tema03/eksempler/eksempel-7.php
+++ tema03/eksempler/eksempel-7.php
@@ -6,10 +6,10 @@ $tall3=$_POST ["tall3"];
 $tall4=$_POST ["tall4"];
 $tall5=$_POST ["tall5"];
 
-$tallarray= array($tall1, $tall2, $tall3, $tall4, $tall5);
+$tallarray=array($tall1, $tall2, $tall3, $tall4, $tall5);
 
 for ($tall=0;$tall<=4;$tall++) { 
-    print("$tellarray[$tall] <br /<");
+    print("$tellarray[$tall]");
    }
     print("<br />");
 
