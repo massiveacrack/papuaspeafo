01dcb54e623d328dc508c76a1ae9a647751d9712
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 16:30:53 +0200

    tesg

--- tema03/eksempler/eksempel-5.php
+++ tema03/eksempler/eksempel-5.php
@@ -4,10 +4,13 @@ $sum=0;
 
 for ($tall=1;$tall<=10;$tall++) { 
      $sum=$sum+$tall;
+     $gjennomsnitt = $sum/10;
     }
 
     {
         print("Summen av tallene 1-10 er lik $sum ");
+        (<br />);
+        print("Gjennomsnittet er $gjennomsnitt");
     }
 
 ?>
