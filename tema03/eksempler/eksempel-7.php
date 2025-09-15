edff5c0dcd80b3622df5382bd14ad2e002755611
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-15 17:28:27 +0200

    tset

--- tema03/eksempler/eksempel-7.php
+++ tema03/eksempler/eksempel-7.php
@@ -6,16 +6,14 @@ $tall3=$_POST ["tall3"];
 $tall4=$_POST ["tall4"];
 $tall5=$_POST ["tall5"];
 
-$tallarray=array($tall1, $tall2, $tall3, $tall4, $tall5);
+$tallArray=array($tall1, $tall2, $tall3, $tall4, $tall5);
 
 for ($tall=0;$tall<=4;$tall++) { 
-    print("$tellarray[$tall]");
+    print($tellArray[$tall]);
    }
     print("<br />");
 
 for ($tall=4;$tall>=0;$tall--) { 
-    print("$tellarray[$tall]");
+    print($tellArray[$tall]);
    }
-
-   
 ?>
