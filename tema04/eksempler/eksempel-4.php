a8a1efe181400ff9e36356f007dac864d9f712e2
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-28 20:06:23 +0200

    e

---
+++ tema04/eksempler/eksempel-4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Eksempel 4</title>
+</head>
+<body>
+  <h3>Eksempel 4 (validering av klassekode) </h3>
+    
+  <form method="post" action="tema04/eksempler/eksempel-4.php" id="registrerKlassekodeSkjema" name="registrerKlassekodeSkjema" >
+    Klassekode <input type="text" name="klassekode" id="klassekode"> <font color="#FF0000">*</font> <em> (klassekode skal inneholde 2 bokstaver og 1 siffer)</em> <br />
+    <input type="submit" value="Registrer klassekode" id="registrerKlassekodeKnapp" name="registrerKlassekodeKnapp" />
+    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+  </form>
+</body>
+</html>
