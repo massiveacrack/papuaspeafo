8387d1c19f78ecffe213272f0d25325eeccd0c31
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-28 20:07:47 +0200

    ge

--- tema04/eksempler/eksempel-4.php
+++ tema04/eksempler/eksempel-4.php
@@ -1,15 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Eksempel 4</title>
-</head>
-<body>
-  <h3>Eksempel 4 (validering av klassekode) </h3>
-    
-  <form method="post" action="tema04/eksempler/eksempel-4.php" id="registrerKlassekodeSkjema" name="registrerKlassekodeSkjema" >
-    Klassekode <input type="text" name="klassekode" id="klassekode"> <font color="#FF0000">*</font> <em> (klassekode skal inneholde 2 bokstaver og 1 siffer)</em> <br />
-    <input type="submit" value="Registrer klassekode" id="registrerKlassekodeKnapp" name="registrerKlassekodeKnapp" />
-    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
-  </form>
-</body>
-</html>
+<?php  /*  Eksempel 2 */
+/*
+/*    Programmet mottar klassekode fra et HTML-skjema 
+/*    Programmet sjekker om klassekode er korrekt fylt ut
+*/
+  $klassekode=$_POST ["klassekode"];
+
+  $lovligKlassekode=true;
+
+  if (!$klassekode)  /* klassekode er ikke fylt ut */
+    {
+      $lovligKlassekode=false;
+      print("Klassekode er ikke fylt ut <br />");
+    }
+  else if (strlen($klassekode)!=3)  /* klassekode består ikke av 3 tegn */
+    {
+      $lovligKlassekode=false;
+      print("Klassekode best&aring;r ikke av 3 tegn <br />");
+    }
+  else
+    {
+      $tegn1=$klassekode[0];   /* første tegn i klassekoden  */
+      $tegn2=$klassekode[1];   /* andre tegn i klassekoden  */
+      $tegn3=$klassekode[2];   /* tredje tegn i klassekoden  */
+
+      if (!ctype_alpha($tegn1))  /* tegn1 er ikke bokstav */ 
+        {
+          $lovligKlassekode=false;
+          print("F&oslash;rste tegn er ikke en bokstav <br />");
+        }
+		
+      if (!ctype_alpha($tegn2))  /* tegn2 er ikke bokstav */
+        {
+          $lovligKlassekode=false;
+          print("Andre tegn er ikke en bokstav <br />");
+        }
+		
+      if (!ctype_digit($tegn3))  /* tegn3 er ikke et siffer */ 
+        {
+          $lovligKlassekode=false;
+          print("Siste tegn er ikke et siffer  <br />");
+        }
+    }
+
+  if ($lovligKlassekode)  /* klassekode er  korrekt fylt ut */
+    {
+      print("Klassekode er korrekt fylt ut <br />");
+    }
+
+?>
