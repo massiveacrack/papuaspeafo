16ce7a4fbd4f5761433d8fc3006e17288b2f81b3
massiveacrack <ludyniafranciszek@gmail.com>
2025-09-28 19:09:35 +0200

    geasgt

---
+++ tema04/eksempler/eksempel-3.php
@@ -0,0 +1,25 @@
+<?php  /*  Eksempel 3 */
+/*
+/*    Programmet mottar postnr fra et HTML-skjema 
+/*    Programmet sjekker om postnr er korrekt fylt ut
+*/
+  $postnr=$_POST ["postnr"];
+
+  if (!$postnr)  /* postnr er ikke fylt ut */
+    {
+      print("Postnr er ikke fylt ut <br />");
+    }
+  else if (strlen($postnr)!=4)  /* postnr bestĺr ikke av 4 tegn */
+    {
+          print("Postnr best&aring;r ikke av 4 tegn <br />");
+    }
+  else if (!ctype_digit($postnr))  /* minst ett av tegnene er ikke et siffer */
+    {
+       print("Postnr best&aring;r ikke bare av siffre  <br />");
+    }
+else
+	{
+		print("Postnr er korrekt fylt ut <br />");
+	}
+
+?>  
