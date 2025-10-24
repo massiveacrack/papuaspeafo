39eae2482d742a2f56205bbbfca146d670c3874e
massiveacrack <ludyniafranciszek@gmail.com>
2025-10-24 10:58:08 +0200

    gere

--- Obligatorisk_Arbeidskrav.2/db_connect.php
+++ Obligatorisk_Arbeidskrav.2/db_connect.php
@@ -1,8 +1,14 @@
-<?php
-$host = 'localhost';
-$dbname = 'fru6323'; // Replace with your database name
-$username = 'fru632'; // Replace with your username
-$password = ''; // Replace with your password
+<?php  /* db-tilkobling */
+/*
+/*  Programmet foretar tilkobling til database-server og valg av database
+*/
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+
+ $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+    /* tilkobling til database-serveren utført */
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
